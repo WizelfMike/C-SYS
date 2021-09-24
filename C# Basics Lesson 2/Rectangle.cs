d9e43d14b6af4bd46cd02eea483498c4c6ae629a
WizelfMike <70898423+WizelfMike@users.noreply.github.com>
2021-09-24 11:00:21 +0200

    Push to repo, and creation of which has been noted

---
+++ C# Basics Lesson 2/Rectangle.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace ClassPractice2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle rectangle = new Rectangle(0, 5, 4, 6);
+
+            Rectangle cube = new Rectangle(5, 0, "Big");
+
+
+        }
+    }
+
+    class Rectangle
+    {
+        private int x;
+        private int y;
+        private int width;
+        private int height;
+        private string size;
+
+        
+
+        
+        
+        public Rectangle(int x,int y,int width,int height)
+        {
+            this.x = x;
+            this.y = y;
+            this.width = width;
+            this.height = height;
+        }
+
+        public Rectangle(int x, int y, string size)
+        {
+            this.x = x;
+            this.y = y;
+            this.size = size;
+        }
+
+    }
+}
